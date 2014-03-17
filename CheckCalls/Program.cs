6f6107666410cdbda227c83b64b0697f1f91a348
dmaths <mathias.dave1@gmail.com>
2014-03-17 13:03:31 -0400

    Initial Upload

---
+++ CheckCalls/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.IO;
+using System.Data;
+
+namespace CheckCalls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var vConString = ConfigurationManager.ConnectionStrings["DBLOWCon"];
+            string sConn = vConString.ConnectionString;
+            string sProc = System.Configuration.ConfigurationManager.AppSettings["StoredProc"];
+            
+            using (var conn = new SqlConnection(sConn))
+            using (var command = new SqlCommand(sProc, conn)
+            {
+                CommandType = CommandType.StoredProcedure
+            })
+            {
+                conn.Open();
+                command.ExecuteNonQuery();
+                conn.Close();
+            }
+
+        }
+    }
+}
